101b62e51e18e0c58322624bb0140388248326ee
Szczerbo <szczerbowski.maciej@gmail.com>
2019-12-03 15:20:21 +0100

    Section3_Security

---
+++ DatingApp.API/Migrations/20191203093453_UserEntityFieldsRenamed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class UserEntityFieldsRenamed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Passwordhash",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "Passwordhash");
+        }
+    }
+}
